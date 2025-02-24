7177254daef4da2ff287dce9c88be3fd78ed0462
Vitor Nickhorn Kroeff <138133399+vitorkroeff@users.noreply.github.com>
2025-02-24 18:01:13 -0300

    Função de Escrita no Big Query

--- api_moedas_bq.py
+++ api_moedas_bq.py
@@ -1,13 +1,17 @@
-import base64
+# pacotes utilizados
 import pandas as pd
 from datetime import datetime, timedelta
 from bcb import currency
 from google.cloud import bigquery
 
+# função que retorna a cotação de um vetor de moedas.
+# Cotações fornecidas pelo Banco Central do Brasil
 def get_cotacao_moeda(moedas, data_inicio, data_fim):
     dados = currency.get(moedas, start=data_inicio, end=data_fim)
     return dados.reset_index()
 
+
+# Função de Escrita no Big Query
 def import_bq(d_frame, table_name):
     client = bigquery.Client()
     dataset_id = "api_moedas"
@@ -18,6 +22,9 @@ def import_bq(d_frame, table_name):
     job.result()
     print(f"Dados importados: {table_ref.table_id}")
 
+
+# Código a ser executado
+# os parâmetros de event e context são necessários para gatilhos do tipo Pub/Sub
 def main(event, context=None):
     data_inicial = datetime.today() - timedelta(days=1)
     data_final = datetime.today()
@@ -28,4 +35,4 @@ def main(event, context=None):
     )    
     import_bq(dados, "historico_cotacoes")
     print("Processamento concluído!")
-    return "Processamento concluído com sucesso!"
+    return "Processamento concluído com sucesso!" # É necessário um retorno do tipo string, etc, pois ele é utilizado como um endpoint no Flask
